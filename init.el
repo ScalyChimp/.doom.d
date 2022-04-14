06076adede3dc244b6692320cd9ab4625dfd95cf
scaly-chimp <scalychimp@gmail.com>
2022-04-15 00:04:07 +0800

    remove direnv(idk what it does), use editor config

--- init.el
+++ init.el
@@ -86,9 +86,9 @@
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
        (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
-       direnv
+       ;;direnv
        ;;docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
