59dfae6dc9c90b81ae35f6b2ae837b5c9762f6a6
scalychimp <scalychimp@gmail.com>
2022-05-21 21:33:13 +0100

    * config.el (:and): change org mode markers

--- config.el
+++ config.el
@@ -100,3 +100,13 @@
 ;; make C-x o not ignore treemacs side-windows
 (after! (:and treemacs ace-window)
   (setq aw-ignored-buffers (delq 'treemacs-mode aw-ignored-buffers)))
+
+(setq org-hide-emphasis-markers t)
+
+(custom-set-faces
+  '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+)
