912710401b2dfe3d018f023a49ad5b1b889ed684
scalychimp <scalychimp@gmail.com>
2022-07-13 22:31:13 +0100

    * init.el: use ivy instead of vertico

--- init.el
+++ init.el
@@ -23,8 +23,8 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;;ivy               ; a search engine for love and life
-       (vertico +icons)           ; the search engine of the future
+       (ivy +fuzzy +icons +prescient); a search engine for love and life
+       ;; (vertico +icons)           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
