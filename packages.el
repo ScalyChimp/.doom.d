d7de51e580e29be1dcc7b69fdeaff05cfdb541cd
scalychimp <scalychimp@gmail.com>
2023-02-27 16:46:23 +0000

    move package.el and init.el out of literate config

---
+++ packages.el
@@ -0,0 +1,8 @@
+;;; packages.el -*- lexical-binding: t; -*-
+(package! org-modern)
+(package! toml-mode)
+(package! i3wm-config-mode)
+(package! exercism
+  :recipe (:host github
+           :repo "anonimitoraf/exercism.el"))
+(package! org-appear)
